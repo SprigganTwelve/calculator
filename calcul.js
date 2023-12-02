9bcda8c035cffca926bec088e2d5f7b1095d613b
Paterne <angepaterne.dali@gmail.com>
2023-12-02 15:45:09 +0100

    better styling

---
+++ calcul.js
@@ -0,0 +1,6 @@
+var buttonOne = document.getElementById('1');
+var input = document.getElementById('number');
+//target.innerHTLM = elementHtml permet de recuperer un element html comme avec '.value'
+buttonOne.addEventListener('click', function(element){ 
+    input.value = "1";   
+})
